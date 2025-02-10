2abe047bf8c518146afd1e62887a5d2bc1396d47
sacha-c <broute.sacha@gmail.com>
2025-02-10 08:37:43 +0100

    test: type bug

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,7 @@ function RootComponent() {
     <>
       <div className="p-2 flex gap-2 text-lg">
         <Link
-          to="/"
+          to="inexistent"
           activeProps={{
             className: 'font-bold',
           }}
